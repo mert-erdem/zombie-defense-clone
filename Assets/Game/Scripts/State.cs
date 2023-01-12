958684ddcc75a7b05ed0761181011c8b2f5cc93b
Mert <mrterdem1998@hotmail.com>
2023-01-12 17:16:39 +0300

    Base movement and enemy searching

---
+++ Assets/Game/Scripts/State.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Core
+{
+    public class State
+    {
+        public delegate void OnUpdate();
+        public OnUpdate onUpdate;
+
+        public delegate void OnStateEnter();
+        public OnStateEnter onStateEnter;
+
+        public delegate void OnStateExit();
+        public OnStateExit onStateExit;
+
+        public State(OnStateEnter onStateEnter, OnUpdate onUpdate, OnStateExit onStateExit)
+        {
+            this.onStateEnter = onStateEnter ?? (() => { });
+            this.onUpdate = onUpdate ?? (() => { });
+            this.onStateExit = onStateExit ?? (() => { });
+        }
+    }
+}
